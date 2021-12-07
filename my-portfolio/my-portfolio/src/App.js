ffdcf89887a983465fd13bbbb6e930c8666b6e3b
Fox Rigney <foxrigney@gmail.com>
2021-12-06 18:58:50 -0600

    groundwork laid

--- my-portfolio/my-portfolio/src/App.js
+++ my-portfolio/my-portfolio/src/App.js
@@ -3,7 +3,7 @@
 import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
@@ -11,7 +11,7 @@ import Testimonials from "./components/Testimonials";
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
+      {/* <Navbar /> */}
       <About />
       <Projects />
       <Skills />
