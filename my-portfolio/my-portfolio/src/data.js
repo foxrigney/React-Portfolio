d649a8283221b369e7b4769e2abfaf0e6dab3f19
Fox Rigney <foxrigney@gmail.com>
2021-12-07 10:39:53 -0600

    personal projects

--- my-portfolio/my-portfolio/src/data.js
+++ my-portfolio/my-portfolio/src/data.js
@@ -1,35 +1,35 @@
 export const projects = [
   {
-    title: "React Reserve",
+    title: "Project 1",
     subtitle: "MERN Stack",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.youtube.com/watch?v=xHoxkD8kKJg",
   },
   {
-    title: "React Tracks",
+    title: "Project 2",
     subtitle: "React and Python",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.youtube.com/watch?v=gHkM_lmy5X8",
   },
   {
-    title: "DevChat",
+    title: "Project 3",
     subtitle: "React and Firebase",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.youtube.com/watch?v=16i1WNmXn5Y",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "Project 4",
+    subtitle: "Cactus",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.youtube.com/watch?v=Qi1KebO4bzc",
   },
 ];
 
@@ -38,14 +38,14 @@ export const testimonials = [
     quote:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
     image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
+    name: "Schmoopy Doopy",
     company: "Zoozle",
   },
   {
     quote:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
     image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
+    name: "Megatron",
     company: "Dooble.io",
   },
 ];
